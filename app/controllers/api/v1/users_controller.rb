93f3c118423f570efa90d742484258037c846fbc
Andre Oliveira <andre.oliveira-impulso@m4u.com.br>
2020-03-21 12:18:12 -0400

    (new feature) initializing methods

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index; end
+
+      def show; end
+
+      def destroy; end
+    end
+  end
+end
