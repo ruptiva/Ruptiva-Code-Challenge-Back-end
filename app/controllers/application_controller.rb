2cb37935d6f016927812958928ea9c48502d4190
Andre Oliveira <andre.oliveira-impulso@m4u.com.br>
2020-03-21 12:15:58 -0400

    (new feature) answering only with json and permitting first name and last name on registration devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  respond_to :json
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
 end
