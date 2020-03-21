6c47e65cf1f20fddbdf103d4bf7d5c15c3290ac9
Andre Oliveira <andre.oliveira-impulso@m4u.com.br>
2020-03-21 12:17:15 -0400

    (new feature) no more namespacing devise, only scoping it coz was confusing devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  scope :api, defaults: { format: :json } do
+    scope :v1 do
+      devise_for :users, skip: :passwords
+    end
+  end
+
   namespace :api do
     namespace :v1 do
-      devise_for :users, skip: :passwords
       resources :users, only: %i[index show destroy]
     end
   end
