e455f8e50c66293d192ebe11675a947ca7607263
Andre Oliveira <andre.oliveira-impulso@m4u.com.br>
2020-03-21 15:49:23 -0400

    (new feature) configuring token on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  acts_as_token_authenticatable
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
