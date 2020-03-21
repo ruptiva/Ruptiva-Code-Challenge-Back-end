7a13c014890d3662a9b5ec10a79a33ada45d38fd
Andre Oliveira <andre.oliveira-impulso@m4u.com.br>
2020-03-21 17:14:13 -0400

    (new feature) doing the RUD on user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,11 +3,43 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      def index; end
+      before_action :load_users, only: :index
+      before_action :load_user, only: %i[show update destroy]
 
-      def show; end
+      def index
+        success_response(data: @users, model: 'User')
+      end
 
-      def destroy; end
+      def show
+        success_response(data: @user, model: 'User')
+      end
+
+      def update
+        if @user.update(user_params)
+          success_response(data: @user, model: 'User')
+        else
+          error_response(data: @user)
+        end
+      end
+
+      def destroy
+        @user.destroy
+        head :no_content
+      end
+
+      private
+
+      def load_users
+        @users = User.all
+      end
+
+      def load_user
+        @user = User.find(params.dig(:id))
+      end
+
+      def user_params
+        params.require(:user).permit(:first_name, :last_name)
+      end
     end
   end
 end
