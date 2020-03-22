d949e495eddfd48e81cd5fa7fffcaddc2d4b63ac
Andre Oliveira <andre.oliveira-impulso@m4u.com.br>
2020-03-22 18:26:42 -0400

    (new feature) seeding the ruptiva user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,5 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.first_or_create(
+  first_name: 'Maikel', last_name: 'Bald', email: 'maikel@ruptiva.com',
+  password: 'ilikeruptiva', role: :admin,
+  authentication_token: 'kkZxYyU3eBXSsXE1Jyb3'
+)
